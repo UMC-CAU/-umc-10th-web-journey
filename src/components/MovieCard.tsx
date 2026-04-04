432733fc84031bc40a8805b2570622ff4df9cea6
02juwon <102983484+02juwon@users.noreply.github.com>
2026-04-05 02:38:20 +0900

    fix : 수정 완료

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -3,7 +3,7 @@ import type { Movie } from '../types/movie';
 
 interface MovieCardProps {
     movie: Movie;
-
+}
 
 export default function MovieCard({ movie }: MovieCardProps) {
     const [isHovered, setIsHovered] = useState(false);
